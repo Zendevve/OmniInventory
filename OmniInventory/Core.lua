bcdd3c0cabe7ffef1be2bf5af93074f2953e2271
Zendevve <nathanielguinto6@gmail.com>
2026-06-29 15:06:27 +0800

    Correct author attribution to Zendevve

--- OmniInventory/Core.lua
+++ OmniInventory/Core.lua
@@ -14,7 +14,7 @@ local addonName, Omni = ...
 _G.OmniInventory = Omni
 
 Omni.version = "2.0-alpha"
-Omni.author = "Synastria"
+Omni.author = "Zendevve"
 
 -- =============================================================================
 -- Event Handler
